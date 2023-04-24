1f087b0d6dbd44f92b795e500b35d0e028465011
r3z000 <114754897+r3z000@users.noreply.github.com>
2023-04-24 05:13:52 +0400

    added navbar, dark/light modes, toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +23,13 @@ export default function RootLayout({
       className={cn("bg-white text-slate-900 antialiased", inter.className)}
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
-        <Providers>{children}</Providers>
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          <Toaster position="bottom-right" />
+
+          <main>{children}</main>
+        </Providers>
 
         {/* Allow more height for mobile menu on mobile */}
         <div className="h-40 md:hidden" />
